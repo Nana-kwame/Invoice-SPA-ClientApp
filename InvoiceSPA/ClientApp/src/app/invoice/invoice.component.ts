11cb15d1b5e5b0d4d0ebd86e728713509b4dce14
Peter Asamoah <peter.asamoah7@outlook.com>
2019-03-31 16:54:12 +0100

    Create Invoice form

--- InvoiceSPA/ClientApp/src/app/invoice/invoice.component.ts
+++ InvoiceSPA/ClientApp/src/app/invoice/invoice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceComponent implements OnInit {
 
-  constructor() { }
+  public invoiceForm: FormGroup;
+  public invoiceItems: FormArray;
+  public authorities: FormArray;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createInvoiceForm();
   }
 
+  private createInvoiceForm() {
+    this.invoiceForm = this.formBuilder.group({
+      recipientName: '',
+      recipientAddress: '',
+      invoiceTitle: '',
+      invoiceNo: '',
+      invoiceItems: this.formBuilder.array([this.createInvoiceItem()]),
+      total: '',
+      authorities: this.formBuilder.array([this.creatAuthority()])
+    });
+  }
+
+  private createInvoiceItem(): FormGroup {
+    return this.formBuilder.group({
+      itemName: '',
+      itemDescription: '',
+      itemAmount: '',
+    });
+  }
+
+  private creatAuthority(): FormGroup {
+    return this.formBuilder.group({
+      authorityName: '',
+      authorityDepartment: '',
+    });
+  }
+
+  public addInvoiceItem() {
+    this.invoiceItems = this.invoiceForm.get('invoiceItems') as FormArray;
+    this.invoiceItems.push(this.createInvoiceItem());
+  }
+
+  public removeInvoiceItem(i) {
+    this.invoiceItems = this.invoiceForm.get('invoiceItems') as FormArray;
+    this.invoiceItems.removeAt(i);
+  }
+
+  public addAuthority() {
+    this.authorities = this.invoiceForm.get('authorities') as FormArray;
+    this.authorities.push(this.creatAuthority());
+  }
+
+  public removeAuthority(i) {
+    this.authorities = this.invoiceForm.get('authorities') as FormArray;
+    this.authorities.removeAt(i);
+  }
 }
